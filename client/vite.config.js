4fcc58add365f7bd3c311aaf359ef47278bbd76f
Hassan Hossam <egoker1234@gmail.com>
2023-09-30 15:57:38 +0300

    add some edits

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://goldgym-kfel.onrender.com'
-    }
-  },
+  server: { },
   plugins: [react()]
 })
