bc90ca40c79131914831ffb69daf4660842c2927
Hassan Hossam <egoker1234@gmail.com>
2024-04-23 00:38:50 +0200

    updating

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: { },
+  build: { manifest: true, outDir: './dist' },
+  base: '/',
+  root: './src',
+  server: {},
   plugins: [react()]
 })
