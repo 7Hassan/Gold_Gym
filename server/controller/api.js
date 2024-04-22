0a00f33b8316a1bde0dcf6faef10cd7f791dbf85
Hassan Hossam <egoker1234@gmail.com>
2024-04-23 01:22:51 +0200

    updating

--- server/controller/api.js
+++ server/controller/api.js
@@ -5,6 +5,7 @@ const catchError = require('../Errors/catch')
 exports.filterExercises = catchError(async (req, res, next) => {
   const { id, bodyPart, target, equipment } = req.body
   const languageCode = req.cookies.i18next;
+  console.log('🚀 ~ req.cookies:', req.cookies)
   let exercises, query = {};
   if (id) query._id = id;
   if (bodyPart) query.bodyPart = bodyPart;
